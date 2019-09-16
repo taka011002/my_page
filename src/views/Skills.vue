45db9bd0f55c6fa17d1feae0330d5e67705716e1
Piece123 <taka.rock.i4@gmail.com>
2019-09-17 02:32:23 +0900

    コンテンツ調整

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -9,7 +9,7 @@
                             <h5>メイン</h5>
                             <p>Ruby(Ruby on Rails), Swift(iOS Application)</p>
                             <h5>経験あり</h5>
-                            <p>Go, Python, Kotlin(Android), Java(Android, Spring Boot), C#(ASP.NET Core, Unity), JS(jQuery, Vue, Node-RED), HTML, CSS, Bootstrap</p>
+                            <p>Go, Python, Kotlin(Android), Java(Android, Spring Boot), C#(ASP.NET Core, Unity), PHP, JS(jQuery, Vue, Node-RED), HTML, CSS, Bootstrap</p>
                             <p>Docker, git, PostgreSQL, MySQL, AWS, GCP, Firebase</p>
 
                             <h4>大学にて</h4>
@@ -19,9 +19,15 @@
                             <p>基本情報技術者試験</p>
 
                             <h4>興味あり</h4>
-                            <p>設計パターン, Clean Architecture, ドメイン駆動開発</p>
+                            <p>設計パターン, Clean Architecture, ドメイン駆動開発, Microservice</p>
 
-                        </b-card-text>
+                            <p>個人開発だとRubyやPythonといったすぐに動くものが作れる動的型付け言語が好きです。<br>
+                                しかし、ある程度の規模以上の開発では
+                                書く人によりプログラムの質が左右されやすい為、静的型付けかつ型推論、null安全なSwift, Kotlinといった言語が好きです。<br>
+                                その為、Goにも今後力を入れたいと考えてます。
+                            </p>
+
+                       </b-card-text>
                     </b-card-body>
                 </b-card>
             </div>
