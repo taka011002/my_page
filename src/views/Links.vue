caff78ace4c89001a9be4d8e98721fa207e07007
Piece123 <taka.rock.i4@gmail.com>
2019-11-04 20:21:00 +0900

    画像を軽量化

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -8,17 +8,17 @@
                             <b-row>
                                 <b-col cols="4">
                                     <b-link href="https://github.com/taka011002">
-                                        <b-img src="icons/GitHub-Mark-120px-plus.png" style="max-width: 100px;"></b-img>
+                                        <b-img src="icons/github_logo.png" style="max-width: 100px;"></b-img>
                                     </b-link>
                                 </b-col>
                                 <b-col cols="4">
                                     <b-link href="https://twitter.com/taka0110_">
-                                        <b-img src="icons/Twitter_Social_Icon_Circle_Color.png" style="max-width: 100px;"></b-img>
+                                        <b-img src="icons/twitter_logo.png" style="max-width: 100px;"></b-img>
                                     </b-link>
                                 </b-col>
                                 <b-col cols="4">
                                     <b-link href="https://www.facebook.com/takahiro.ishitsuka.9">
-                                        <b-img src="icons/f_logo_RGB-Blue_1024.png" style="max-width: 100px;"></b-img>
+                                        <b-img src="icons/facebook_logo_png" style="max-width: 100px;"></b-img>
                                     </b-link>
                                 </b-col>
                             </b-row>
