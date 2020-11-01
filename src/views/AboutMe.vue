6b74aa00a05569db084ca3bd289883401b8cce75
Piece123 <taka.rock.i4@gmail.com>
2020-11-01 15:56:33 +0900

    画像を更新

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -5,7 +5,7 @@
         <b-card no-body class="overflow-hidden mx-auto">
           <b-row no-gutters>
             <b-col md="4">
-              <b-card-img-lazy fluid v-bind="lazyImage" src="profile_image.jpg" class="profile-image" alt="profile_image"></b-card-img-lazy>
+              <b-card-img-lazy fluid v-bind="lazyImage" src="tokusatsu_kaiju_black.png"> class="profile-image" alt="profile_image"></b-card-img-lazy>
             </b-col>
             <b-col md="8">
               <b-card-body>
@@ -14,7 +14,7 @@
 
                   <p>
                     エンジニアとして活動している大学生です。<br>
-                    長期インターンを複数社にて経験し、業務として約2年エンジニアとして携わっております。<br>
+                    長期インターンを複数社にて経験し、業務として約2年半ほどエンジニアとして携わっております。<br>
                     分野としてはフロントエンド, ネイティブアプリ, サーバーサイド, インフラと幅広く触れております。<br>
                     新規事業の立ち上げやスタートアップでのインターン等経験があります。<br>
                   </p>
@@ -26,6 +26,9 @@
                     </li>
                     <li>
                       Swiftを用いたネイティブアプリ開発(iOS)<br>業務にて約半年程の開発経験があります。アーキテクチャとしてはVIPERを用いての開発を経験致しました。
+                    </li>
+                    <li>
+                        AWS上のインフラの構築
                     </li>
                     <li>
                       他にも複数技術の使用経験がございます。未経験の技術であっても苦手意識を持たず、短期間でのキャチアップができます。
